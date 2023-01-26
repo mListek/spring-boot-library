0639a3170e8fb0a45ffd3e3d65b00d15c10711b6
mListek <michal.listex@gmail.com>
2023-01-26 18:56:26 +0100

    created repository

---
+++ src/main/java/com/listek/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,9 @@
+package com.listek.springbootlibrary.dao;
+
+import com.listek.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
