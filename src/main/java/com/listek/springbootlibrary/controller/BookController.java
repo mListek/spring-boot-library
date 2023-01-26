b2be3174030729e782371ac944e8f9c52177a67f
mListek <michal.listex@gmail.com>
2023-01-26 19:19:54 +0100

    created controller

---
+++ src/main/java/com/listek/springbootlibrary/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.listek.springbootlibrary.controller;
+
+import com.listek.springbootlibrary.entity.Book;
+import com.listek.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
