7a9bc7fd6462219b1bf8283950e01f3a2b7a6899
mListek <michal.listex@gmail.com>
2023-01-28 21:06:42 +0100

    created model

---
+++ src/main/java/com/listek/springbootlibrary/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,17 @@
+package com.listek.springbootlibrary.responsemodels;
+
+import com.listek.springbootlibrary.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+    private Book book;
+
+    private int daysLeft;
+}
