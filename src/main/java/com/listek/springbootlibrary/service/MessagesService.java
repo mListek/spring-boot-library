987e01f8119f9cddf0a256ab33867af90d193dd4
mListek <michal.listex@gmail.com>
2023-01-30 16:41:15 +0100

    created service

---
+++ src/main/java/com/listek/springbootlibrary/service/MessagesService.java
@@ -0,0 +1,25 @@
+package com.listek.springbootlibrary.service;
+
+import com.listek.springbootlibrary.dao.MessageRepository;
+import com.listek.springbootlibrary.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessagesService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+}
